43df831ef6bb6a18c8444a9234a8cd4488292ba7
Zixiong Zhuang <u7192444@anu.edu.au>
2025-02-03 01:21:32 +1100

    plotting for-loop working

--- R/02-ploting-biomass.R
+++ R/02-ploting-biomass.R
@@ -6,10 +6,8 @@ source("functions/fn-plotting.R") # has my functions
 
 biomass_2022 <- readRDS("data/data-input/biomass_2022.rds")
 
-#levels(as.factor(biomass_2022$genotype))[1]
-
 current_genotype <- biomass_2022 |> filter(genotype == "AAC_Goodwin")
-anova_result <- aov(above_ground_biomass ~ temperature_level*co2_level, data = current_genotype)
+aov1 <- aov(above_ground_biomass ~ temperature_level*co2_level, data = current_genotype)
 #summary(aov1)
 
 p_vals <- tidy(aov1)$p.value[1:3]
@@ -30,7 +28,7 @@ biomass_2022 |>
   #facet_wrap(~genotype) +
   scale_fill_manual(values = c("white", "gray")) +
   labs(y="Total above ground biomass (g)", size = 5) +
-  scale_y_continuous(breaks=seq(5,35,5), limits = c(0,40),expand=c(0,0), sec.axis = dup_axis(name = NULL, labels = NULL)) +
+  scale_y_continuous(breaks=seq(5,40,5), limits = c(0,45),expand=c(0,0), sec.axis = dup_axis(name = NULL, labels = NULL)) +
   theme(axis.title.y = element_text(size=12), 
         axis.text.y = element_text(size =12, margin=margin(3,3,3,3,"pt"), colour=c("black", "black", "black", "black")),
         axis.title.x = element_blank(), 
@@ -43,8 +41,8 @@ biomass_2022 |>
         axis.ticks.length.y.right = unit(0, "cm"),
         axis.ticks.length.x.bottom = unit(0.25, "cm"),  # Add ticks at the bottom x-axis
         axis.ticks.length.y.left = unit(0.25, "cm")) +
-  annotate("text", x= 0.6, y=38, label="(a)",size = 5) +
-  annotate("text", x= 1.2, y=35, label=sig_labs,size = 4.8,hjust=0) +
+  annotate("text", x= 0.7, y=42, label="(a)",size = 5) +
+  annotate("text", x= 1.2, y=37, label=sig_labs, size = 4.2, hjust = 0) +
   theme(legend.position = c(0.8, 0.85),
         legend.text = element_text(size = 12),
         legend.title = element_blank(),
@@ -58,8 +56,21 @@ biomass_2022 |>
 # Writing functions -------------------------------------------------------
 
 create_biomass_plot <- function(data, genotype_name, plot_label = "(a)") {
-  data |> 
-    filter(genotype == genotype_name) |> 
+  genotype_data <- data |> 
+    filter(genotype == genotype_name)
+  
+  # Then perform ANOVA on the filtered data
+  aov_result <- aov(above_ground_biomass ~ temperature_level*co2_level, data = genotype_data)
+  
+  p_vals <- tidy(aov_result)$p.value[1:3]
+  
+  sig_labs <- paste(
+    "T:", sig_level(p_vals[1]),
+    "\nCO2:", sig_level(p_vals[2]),
+    "\nT*CO2:", sig_level(p_vals[3])
+  )
+  
+  genotype_data |> 
     ggplot(aes(temperature_level, above_ground_biomass, fill = co2_level)) +
     geom_boxplot(width = 0.24, outliers = FALSE) +
     geom_point(position = position_dodge(width = 0.24)) +
@@ -67,8 +78,8 @@ create_biomass_plot <- function(data, genotype_name, plot_label = "(a)") {
     scale_fill_manual(values = c("white", "gray")) +
     labs(y = "Total above ground biomass (g)", size = 5) +
     scale_y_continuous(
-      breaks = seq(5, 35, 5), 
-      limits = c(0, 40),
+      breaks = seq(5, 40, 5), 
+      limits = c(0, 45),
       expand = c(0, 0), 
       sec.axis = dup_axis(name = NULL, labels = NULL)
     ) +
@@ -96,7 +107,8 @@ create_biomass_plot <- function(data, genotype_name, plot_label = "(a)") {
       axis.ticks.length.x.bottom = unit(0.25, "cm"),
       axis.ticks.length.y.left = unit(0.25, "cm")
     ) +
-    annotate("text", x = 0.6, y = 38, label = plot_label, size = 5) +
+    annotate("text", x= 0.7, y=42, label="(a)",size = 5) +
+    annotate("text", x= 1.2, y=37, label=sig_labs, size = 4.2, hjust = 0) +
     theme(
       legend.position = c(0.8, 0.85),
       legend.text = element_text(size = 12),
@@ -118,7 +130,7 @@ create_biomass_plot <- function(data, genotype_name, plot_label = "(a)") {
     )
 }
 
-create_biomass_plot(biomass_2022, "AAC_Goodwin", "(a)")
+create_biomass_plot(biomass_2022, "A1614-KE07", "(a)")
 
 distinct(biomass_2022,genotype)
 
@@ -142,10 +154,20 @@ for(i in 1:nrow(genotypes)) {
   )
 }
 
+# Don't show y-axis title except for the left most plot
+show_y_labels <- rep(c(TRUE, FALSE, FALSE, FALSE), 5)
+plot_list_modified <- map2(plot_list, show_y_labels, function(p, show) {
+  if (!show) {
+    p + theme(axis.title.y = element_blank(),
+              axis.text.y = element_blank(),
+              axis.ticks.y = element_blank())
+  } else {
+    p
+  }
+})
 
-p1 <- plot_grid(plotlist = plot_list, nrow = 5, ncol = 4)
 
-p1 <- wrap_plots(plot_list, nrow = 5, ncol = 4)
+p1 <- patchwork::wrap_plots(plot_list_modified, nrow = 5, ncol = 4)
 pdf("outputs/plots/p1.pdf", width = 16, height = 16)
 print(p1)
 dev.off()
